9903783c269cf240091030dd3436feb0231d8455
jwalker04 <bradjwalker5@gmail.com>
2015-10-06 16:36:43 -0400

    SwitchBoard Form
    SwitchBoard form with minimal code, names of every other form need to be
    added

---
+++ MCC/MCC/frmSwitchBoard.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSwitchBoard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEmployees = New System.Windows.Forms.Button()
+        Me.btnMembers = New System.Windows.Forms.Button()
+        Me.btnSales = New System.Windows.Forms.Button()
+        Me.btnInventory = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEmployees
+        '
+        Me.btnEmployees.Location = New System.Drawing.Point(52, 60)
+        Me.btnEmployees.Name = "btnEmployees"
+        Me.btnEmployees.Size = New System.Drawing.Size(75, 23)
+        Me.btnEmployees.TabIndex = 0
+        Me.btnEmployees.Text = "Employees"
+        Me.btnEmployees.UseVisualStyleBackColor = True
+        '
+        'btnMembers
+        '
+        Me.btnMembers.Location = New System.Drawing.Point(160, 60)
+        Me.btnMembers.Name = "btnMembers"
+        Me.btnMembers.Size = New System.Drawing.Size(75, 23)
+        Me.btnMembers.TabIndex = 1
+        Me.btnMembers.Text = "Members"
+        Me.btnMembers.UseVisualStyleBackColor = True
+        '
+        'btnSales
+        '
+        Me.btnSales.Location = New System.Drawing.Point(52, 114)
+        Me.btnSales.Name = "btnSales"
+        Me.btnSales.Size = New System.Drawing.Size(75, 23)
+        Me.btnSales.TabIndex = 2
+        Me.btnSales.Text = "Sales"
+        Me.btnSales.UseVisualStyleBackColor = True
+        '
+        'btnInventory
+        '
+        Me.btnInventory.Location = New System.Drawing.Point(160, 114)
+        Me.btnInventory.Name = "btnInventory"
+        Me.btnInventory.Size = New System.Drawing.Size(75, 23)
+        Me.btnInventory.TabIndex = 3
+        Me.btnInventory.Text = "Inventory"
+        Me.btnInventory.UseVisualStyleBackColor = True
+        '
+        'frmSwitchBoard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnInventory)
+        Me.Controls.Add(Me.btnSales)
+        Me.Controls.Add(Me.btnMembers)
+        Me.Controls.Add(Me.btnEmployees)
+        Me.Name = "frmSwitchBoard"
+        Me.Text = "frmSwitchBoard"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnEmployees As System.Windows.Forms.Button
+    Friend WithEvents btnMembers As System.Windows.Forms.Button
+    Friend WithEvents btnSales As System.Windows.Forms.Button
+    Friend WithEvents btnInventory As System.Windows.Forms.Button
+End Class
---
+++ MCC/MCC/frmSwitchBoard.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmSwitchBoard
+
+    Private Sub btnEmployees_Click(sender As Object, e As EventArgs) Handles btnEmployees.Click
+        '
+        ' Add Employee Form
+        '
+
+    End Sub
+
+    Private Sub btnMembers_Click(sender As Object, e As EventArgs) Handles btnMembers.Click
+        '
+        ' Add Members Form
+        '
+
+    End Sub
+
+    Private Sub btnSales_Click(sender As Object, e As EventArgs) Handles btnSales.Click
+        '
+        ' Add Sales Form
+        '
+
+    End Sub
+
+    Private Sub btnInventory_Click(sender As Object, e As EventArgs) Handles btnInventory.Click
+        '
+        ' Add Inventory Form
+        '
+
+    End Sub
+End Class
